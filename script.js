903ca7c12a9409a243fddfa0ef3f84377d9591ac
SANDEEP KRISH <168737689+Sandeepkrish8@users.noreply.github.com>
2026-01-19 13:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+ const inputs = document.querySelectorAll(".code");
+
+    inputs[0].focus();
+
+    inputs.forEach((input, index) => {
+      input.addEventListener("keydown", (e) => {
+        // Allow only numbers and backspace
+        if (
+          e.key !== "Backspace" &&
+          isNaN(e.key)
+        ) {
+          e.preventDefault();
+        }
+      });
+
+      input.addEventListener("input", (e) => {
+        if (input.value && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+          if (input.value === "" && index > 0) {
+            inputs[index - 1].focus();
+            inputs[index - 1].value = "";
+          }
+        }
+      });
+    });
